bc8d6f214d9dc361a916415c24f92393b38e8acd
ValenDtv <staryou@mail.ru>
2019-03-23 10:20:29 +0300

    Модель таблицы users и регестрация
    Сделал регистрацию

--- Project/MallHelper/models.py
+++ Project/MallHelper/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class users (models.Model):
+    userid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    class Meta:
+        db_table = "users"
--- Project/MallHelper/views.py
+++ Project/MallHelper/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import users
+from django.http import HttpResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def register(request):
+    request_data = json.loads(request.body)
+    new_user = users(name=request_data['name'], password=request_data['password'], email=request_data['email'])
+    new_user.save()
+    response = {'success': True, 'userid': new_user.userid}
+    response = json.dumps(response)
+    return HttpResponse(response)
